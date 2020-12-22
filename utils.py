bf1cd146d4715f367c559042d911109f59cdbb95
Amos Kendall <apkendall10@gmail.com>
2020-12-22 10:48:44 -0500

    more utils

--- utils.py
+++ utils.py
@@ -1,8 +1,17 @@
 import datetime as dt
+import pandas as pd
 import os
 
-def format_fpath(type):
-    fname_base = 'projection' if type == 'proj' else 'lineup'
-    folder = 'Projections' if type == 'proj' else 'Lineups'
-    fname = '{}_{}.csv'.format(fname_base, dt.date.today())
+type_map = {
+    'line': ('Lineups', 'lineup'),
+    'proj': ('Projection', 'Projections'),
+    'stat': ('Stats', 'stats')
+}
+
+def format_fpath(type, target_date = dt.date.today()):
+    fname_base, folder = type_map[type]
+    fname = '{}_{}.csv'.format(fname_base, target_date)
     return os.path.join('..', folder, fname)
+
+def team_map():
+    return pd.read_csv('team_map.csv').set_index('City').code.to_dict()
