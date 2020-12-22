817e96fc8867fbaf0cace45b59af4e3674a5b526
Amos Kendall <apkendall10@gmail.com>
2020-12-22 18:04:11 -0500

    bug fix

--- getBoxStats.py
+++ getBoxStats.py
@@ -4,31 +4,35 @@ import numpy as np
 import argparse
 from utils import format_fpath
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-d', dest = 'date', default = str(dt.date.today() - dt.timedelta(days = 1)))
-args = parser.parse_args()
+def boxStats(date):
+    try:
+        url = 'https://www.basketball-reference.com/boxscores/?month={}&day={}&year={}'.format(date.month, date.day, date.year)
+        tables = pd.read_html(url)
 
-year, month, day = [int(x) for x in args.date.split('-')]
-date = dt.date(year, month, day)
+        team_map = pd.read_csv('team_map.csv').set_index('City').code.to_dict()
+        games = [team_map[tables[x].iloc[1,0]] for x in range(0,len(tables)-3,3)]
+        url_base = 'https://www.basketball-reference.com/boxscores/{}0{}.html'
+        home_full_index = 0
+        away_full_index = 8
 
-url = 'https://www.basketball-reference.com/boxscores/?month={}&day={}&year={}'.format(date.month, date.day, date.year)
-team_map = pd.read_csv('team_map.csv').set_index('City').code.to_dict()
+        stats = None
+        for g in games:
+            t = pd.read_html(url_base.format(date.strftime('%Y%m%d'),g))
+            for idx in [home_full_index, away_full_index]:
+                temp = t[idx]
+                temp.columns = temp.columns.droplevel(0)
+                temp = temp.set_index('Starters').drop('Reserves').drop('Team Totals')
+                stats = temp if stats is None else stats.append(temp)
 
-tables = pd.read_html(url)
+        stats.drop(stats[stats.MP.str[:3] == 'Did'].index, inplace=True)
+        stats['FP'] = stats.PTS.astype('int') + stats.TRB.astype('int') * 1.2 + stats.AST.astype('int') * 1.5 + stats.BLK.astype('int') * 3 + stats.STL.astype('int') * 3 - stats.TOV.astype('int')
+        stats.to_csv(format_fpath('stat', date))
+    except:
+        print('No games for {}'.format(date))
 
-games = [team_map[tables[x].iloc[1,0]] for x in range(0,len(tables)-3,3)]
-url_base = 'https://www.basketball-reference.com/boxscores/{}0{}.html'
-home_full_index = 0
-away_full_index = 8
-stats = None
-for g in games:
-  t = pd.read_html(url_base.format(date.strftime('%Y%m%d'),g))
-  for idx in [home_full_index, away_full_index]:
-    temp = t[idx]
-    temp.columns = temp.columns.droplevel(0)
-    temp = temp.set_index('Starters').drop('Reserves').drop('Team Totals')
-    stats = temp if stats is None else stats.append(temp)
-
-stats.drop(stats[stats.MP.str[:3] == 'Did'].index, inplace=True)
-stats['FP'] = stats.PTS.astype('int') + stats.TRB.astype('int') * 1.2 + stats.AST.astype('int') * 1.5 + stats.BLK.astype('int') * 3 + stats.STL.astype('int') * 3 - stats.TOV.astype('int')
-stats.to_csv(format_fpath('stat', date))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', dest = 'date', default = str(dt.date.today() - dt.timedelta(days = 1)))
+    args = parser.parse_args()
+    year, month, day = [int(x) for x in args.date.split('-')]
+    date = dt.date(year, month, day)
