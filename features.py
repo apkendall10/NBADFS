6ec9467d30fd1996d4c9c1febd868e97c4377456
Amos Kendall <apkendall10@gmail.com>
2020-12-29 09:36:53 -0600

    feature updates

--- features.py
+++ features.py
@@ -1,10 +1,41 @@
 import numpy as np
 import pandas as pd
+import datetime as dt
 import os
 from utils import get_games
 
 def game_count(date):
     return len(get_games(date))
 
-def fanduel_data():
-    return pd.read_csv(os.path.join('..','FanDuelData'))
+def game_history(date, lookback):
+    off_def = []
+    url_template = 'https://www.basketball-reference.com/boxscores/?month={}&day={}&year={}'
+    for offset in range(lookback):
+        current_date = date - dt.timedelta(days = offset)
+        url = url_template.format(current_date.month, current_date.day, current_date.year)
+        try:
+            tables = pd.read_html(url)
+        except:
+            continue
+        for game in range(0,len(tables)-3,3):
+            box = tables[game]
+            off_def.append([box.iloc[1,0], box.iloc[1,1], box.iloc[0,0]])
+            off_def.append([box.iloc[0,0], box.iloc[0,1], box.iloc[1,0]])
+    return pd.DataFrame(off_def, columns = ['Offense', 'Score', 'Defense'])
+
+def calc_ratings(df, iterations = 50):
+    offense = df.groupby('Offense').mean().Score.rename('ortg')
+    defense = df.groupby('Defense').mean().Score.rename('drtg')
+    for _ in range(iterations):
+        mapper = df.join(offense, on = 'Offense').join(defense, on = 'Defense')
+        mapper['new-ortg'] = mapper.Score * 2 - mapper['drtg']
+        mapper['new-drtg'] = mapper.Score * 2 - mapper['ortg']
+        if((mapper['new-ortg'] - mapper.ortg).abs().sum() + (mapper['new-drtg'] - mapper.drtg).abs().sum() < 1):
+            print('Stopping early')
+            break
+        mapper.drtg = mapper['new-drtg']
+        mapper.ortg = mapper['new-ortg']
+        offense = mapper.groupby('Offense').mean().ortg
+        defense = mapper.groupby('Defense').mean().drtg
+
+    return offense, defense
